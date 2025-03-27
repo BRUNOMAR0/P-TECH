3b405f8100e0e62bfc55139e811b09d9a1907d89
BRUNOMAR0 <brunodamianmar@gmail.com>
2025-03-27 11:40:22 -0600

    exercise 7
    no

---
+++ Python/7.py
@@ -0,0 +1,13 @@
+def Impcompas():
+    lista= ["Gaddel","Luis","Pablo","Alan","Paul"]
+    for Imp in lista:
+        print(Imp)
+        
+        
+def ImpJefes():
+    lista= ["Sandra Aguayo", "Roberto Mar"]
+    for Imp in lista:
+        print(Imp)
+        
+Impcompas()
+ImpJefes()
